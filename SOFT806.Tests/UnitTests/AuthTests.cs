5a1f88f9c71d9c3d5fedd431d086de4037e35687
llanos1205 <llanos1205@gmail.com>
2024-02-20 14:08:14 +1300

    Added fake tests

--- SOFT806.Tests/UnitTests/AuthTests.cs
+++ SOFT806.Tests/UnitTests/AuthTests.cs
@@ -1,15 +1,31 @@
-﻿namespace SOFT806.Tests.UnitTests;
+﻿using FakeItEasy;
+using Microsoft.AspNetCore.Identity;
+using SOFT703A2.Domain.Models;
+using SOFT703A2.Infrastructure.Persistence;
+using SOFT703A2.Infrastructure.Repositories;
+
+namespace SOFT806.Tests.UnitTests;
 
 public class AuthTests
 {
     [SetUp]
     public void Setup()
     {
-        
     }
 
     [Test]
-    public void Test1()
+    public void RegistrationTest()
+    {
+    }
+
+    [Test]
+    public void AuthenticationTest()
+    {
+        Assert.Pass();
+    }
+
+    [Test]
+    public void UserDataTest()
     {
         Assert.Pass();
     }
--- SOFT806.Tests/UnitTests/ProductTests.cs
+++ SOFT806.Tests/UnitTests/ProductTests.cs
@@ -2,5 +2,36 @@
 
 public class ProductTests
 {
-    
+    [SetUp]
+    public void Setup()
+    {
+        
+    }
+
+    [Test]
+    public void CreateProductsTest()
+    {
+        Assert.Pass();
+    }
+    [Test]
+    public void AddProductsToTrolleyTest()
+    {
+        Assert.Pass();
+    }
+    [Test]
+    public void GetTrolleyInfoTest()
+    {
+        Assert.Pass();
+        
+    }
+    [Test]
+    public void PromoteProductTest()
+    {
+        Assert.Pass();
+    }
+    [Test]
+    public void CheckOutTest()
+    {
+        Assert.Pass();
+    }
 }
